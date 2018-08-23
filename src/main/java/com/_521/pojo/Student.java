08149315684f11320b13e5e760a36d1da60a3e4e
FWYcode <1059301273@qq.com>
2018-08-23 23:04:30 +0800

    Initial commit

---
+++ src/main/java/com/_521/pojo/Student.java
@@ -0,0 +1,26 @@
+package com._521.pojo;
+
+/**
+ * Created by 123 on 2018/8/23.
+ */
+public class Student {
+
+    private  int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/test/Test.java
@@ -0,0 +1,18 @@
+package test;
+
+import com._521.pojo.Student;
+
+/**
+ * Created by 123 on 2018/8/23.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Student stude = new Student();
+        stude.setId(1);
+        stude.setName("ssss");
+        System.out.println(stude.getId());
+       System.out.println(stude.getName());
+
+    }
+}
